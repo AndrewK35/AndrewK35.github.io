3a29603901ccf4ae37cfbf761c501b9acc6e3f96
AndrewK35 <35379532+AndrewK35@users.noreply.github.com>
2018-03-25 16:38:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // When the user scrolls the page, execute myFunction 
 window.onscroll = function() {myFunction()};
 
-// Get the header
-var menu = document.getElementById("mymenu");
+// Get the navbar
+var navbar = document.getElementById("navbar");
 
 // Get the offset position of the navbar
-var sticky = menu.offsetTop;
+var sticky = navbar.offsetTop;
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    menu.classList.add("sticky");
+    navbar.classList.add("sticky")
   } else {
-    menu.classList.remove("sticky");
+    navbar.classList.remove("sticky");
   }
 }
