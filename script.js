61c21980d7c1f92b2b19fdd8f982fae72a323cb5
AndrewK35 <35379532+AndrewK35@users.noreply.github.com>
2018-03-25 16:15:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-jQuery(document).ready(function() {
-  var navOffset = jQuery("nav").offset().top;
-  alert(navOffset);
-  
-});
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
