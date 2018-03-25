e718c0d9b80aaf147a61796870298fc82432e012
AndrewK35 <35379532+AndrewK35@users.noreply.github.com>
2018-03-25 16:19:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 window.onscroll = function() {myFunction()};
 
 // Get the header
-var header = document.getElementById("myHeader");
+var menu = document.getElementById("myHeader");
 
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
+var sticky = menu.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    header.classList.add("sticky");
+    menu.classList.add("sticky");
   } else {
-    header.classList.remove("sticky");
+    menu.classList.remove("sticky");
   }
 }
